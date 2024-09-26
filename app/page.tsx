f94fe4dad8634aa06c09addef979654b61392136
Mkhotamirais <mkhotamirais@gmail.com>
2024-09-27 00:45:09 +0700

    dikit

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
     <div className="min-h-[calc(100vh-8rem)] flex flex-col gap-6 items-center justify-center">
       <div className="text-3xl">Welcome {user?.email}</div>
       <Button asChild size="lg">
-        <Link href="/product">View Products</Link>
+        <Link href="/product">View Product</Link>
       </Button>
     </div>
   );
